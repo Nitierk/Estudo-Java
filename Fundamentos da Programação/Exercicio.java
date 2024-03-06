4f0b448bd3c410a1f1d3e409fab9161eaf46eafc
Kauan <cesarkauanferreira@hotmail.com>
2024-03-06 13:47:46 -0300

    Exercicio2

--- Fundamentos da Programação/Exercicio.java
+++ Fundamentos da Programação/Exercicio.java
@@ -1,12 +1,15 @@
-/*
+
+ /*
 1. Soma dos múltiplos de 3 até x. Crie um algoritmo que leia um número inteiro 'x' e calcule
 a soma de todos os múltiplos de 3 no intervalo de 1 até 'x' utilizando um loop PARA. Exiba o
 resultado.
  */
-
- public class Exercici1{
-    public static void Main(String[] args){
-        
-
+ 
+ public class Exercicio{
+    public static void main(String[] args){
+      System.out.println("Hello, Word!");
     }
  }
+
+
+ 
